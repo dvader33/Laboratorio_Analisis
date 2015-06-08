61fae0b3fe72a8aec4cf4cfc8f1c75f27c365d2d
Vader <vhader33@gmail.com>
2015-06-08 10:46:36 -0600

    Segunda linea de texto

--- src/laboratorio_analisis/Laboratorio_Analisis.java
+++ src/laboratorio_analisis/Laboratorio_Analisis.java
@@ -17,6 +17,7 @@ public class Laboratorio_Analisis {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo");
+        System.out.println("Holamundo numero 2");
     }
     
 }
